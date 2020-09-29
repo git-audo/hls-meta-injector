8099f9f4523727270d0cfc373e1a510b383615b7
gitaudo <gabrieleghiba@icloud.com>
2020-09-29 19:19:57 +0200

    Added ts file summary

--- cmd/main.go
+++ cmd/main.go
@@ -10,16 +10,24 @@ var (
 )
 
 func main() {
-
 	f, err := os.Open("./frag.ts")
 	if err != nil {
 		fmt.Errorf("error opening file, %s", err)
 	}
 
-	var pmtPid uint16
-	var streamPids []uint16
+	stat, err := f.Stat()
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Mpeg-ts file analysis")
 	
-	for i:=0; i<5; i++ {
+	var pmtPid uint16
+	streamsPacketsCount := make(map[uint16]int)
+
+	packetsNum := int(stat.Size())/packetSize
+	
+	for i:=0; i<packetsNum; i++ {
 		packet := make([]byte, packetSize)
 		r, err := f.Read(packet)
 		if err != nil || r != packetSize {
@@ -27,19 +35,26 @@ func main() {
 		}
 
 		pid := ((uint16(packet[1]) & 0x1f) << 8) | uint16(packet[2])
-		cc := uint8(packet[3]) & 0xf
-		fmt.Println("pid", pid, "cc", cc)
+		// cc := uint8(packet[3]) & 0xf
 
 		if pid == 0 {
-			sl := ((uint16(packet[8]) & 0x3) << 8) | uint16(packet[9])
+			// pat packet
+			// sl := ((uint16(packet[8]) & 0x3) << 8) | uint16(packet[9])
 			pmtPid = ((uint16(packet[15]) & 0x1f) << 8) | uint16(packet[16])
-			fmt.Println(" sl", sl, "pmt", pmtPid)			
+		} else if pid == pmtPid {
+			// pmt packet
+			s1 := ((uint16(packet[18]) & 0x3) << 8) | uint16(packet[19])
+			s2 := ((uint16(packet[23]) & 0x3) << 8) | uint16(packet[24])
+			streamsPacketsCount[s1] = 0
+			streamsPacketsCount[s2] = 0
+		} else {
+			// pes packet
+			streamsPacketsCount[pid] += 1
 		}
+	}
 
-		if pid == pmtPid {
-			streamPids = append(streamPids, ((uint16(packet[18]) & 0x3) << 8) | uint16(packet[19]))
-			streamPids = append(streamPids, ((uint16(packet[23]) & 0x3) << 8) | uint16(packet[24]))
-			fmt.Println("streams", streamPids)
-		}
+	fmt.Println(" Total number of packets", packetsNum)
+	for k, v := range streamsPacketsCount { 
+		fmt.Printf(" stream %v packets %v\n", k, v)
 	}
 }
